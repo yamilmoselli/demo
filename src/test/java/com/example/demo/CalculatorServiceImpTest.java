a4b80a248221f7e5a211df46591bf65ce545e3c5
yamil <moselli.yamil.1a@gmail.com>
2025-06-07 01:18:43 -0300

    Tests for sumar() method created and checked

---
+++ src/test/java/com/example/demo/CalculatorServiceImpTest.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+
+import com.example.demo.services.CalculatorServiceImp;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+
+public class CalculatorServiceImpTest {
+
+    CalculatorServiceImp service = new CalculatorServiceImp();
+    Model model = new ExtendedModelMap();
+
+    @Test
+    public void testSumar1() {
+        service.sumar(0, 0, model);
+        Assertions.assertEquals(0.0, model.getAttribute("resultado"));
+    }
+
+    @Test
+    public void testSumar2() {
+        service.sumar(5, 0, model);
+        Assertions.assertEquals(5.0, model.getAttribute("resultado"));
+    }
+
+    @Test
+    public void testSumar3() {
+        service.sumar(8, 4, model);
+        Assertions.assertEquals(12.0, model.getAttribute("resultado"));
+    }
+
+    @Test
+    public void testSumar4() {
+        service.sumar(8, -4, model);
+        Assertions.assertEquals(4.0, model.getAttribute("resultado"));
+    }
+
+    @Test
+    public void testSumar5() {
+        service.sumar(-8, -4, model);
+        Assertions.assertEquals(-12.0, model.getAttribute("resultado"));
+    }
+
+    @Test
+    public void testSumar6() {
+        service.sumar(1000000, 4000000, model);
+        Assertions.assertEquals(5000000.0, model.getAttribute("resultado"));
+    }
+
+}
